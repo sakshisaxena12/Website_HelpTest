e208dd3282a8e1c4684590410259694ec963dfdd
Richard Job <richardjob05@gmail.com>
2020-03-15 16:02:26 +0530

    Added header slides and fixed the nav bar

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,10 +4,24 @@ function submitForm() {
   // but duplicate names are possible in HTML
   // Therefore to work around the issue, enforce the correct index
   var frm = document.getElementsByTagName("FORM");
-  
+
   frm.submit(); // Submit the form
   frm.reset(); // Reset all form data
- return false;
- 
-  
+  return false;
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
