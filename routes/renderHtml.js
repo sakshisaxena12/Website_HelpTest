964568f3ef3ab847395fee32a7993ca7db0b2e91
Richard Job <richardjob05@gmail.com>
2020-04-13 14:38:13 +0530

    fixed search feature

--- routes/renderHtml.js
+++ routes/renderHtml.js
@@ -1,35 +1,41 @@
-var express = require('express');
+var express = require("express");
 var renderHtml = express.Router();
-var path = require('path');
-var fs = require('fs');
+var path = require("path");
+var fs = require("fs");
 
-renderHtml.route('*')
-.get((req, res) => {
+renderHtml.route("*").get((req, res) => {
+  let fileUrl;
+  if (req.url == "/") fileUrl = "/main";
+  else fileUrl = req.url;
 
-    let fileUrl;
-    if (req.url == '/') fileUrl = '/main';
-    else fileUrl = req.url;
-  
-    let filePath = path.resolve('./public/html/' + fileUrl +'.html')
-    const ext = path.extname(filePath)
-    if (ext === '.html') {
-      fs.exists(filePath, (exist) => {
-        if (!exist) {
-          res.statusCode = 404;
-          res.setHeader('Content-Type', 'text/html')
-          res.end('<html><body><h1>Eror 404</h1></body></html>')
-          return
-        }
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        fs.createReadStream(filePath).pipe(res)
-      })
-    } else {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/html')
-      res.end('<html><body><h1>Eror 404 not a HTML file</h1></body></html>')
-    }
-  })
+  if (fileUrl.includes("?")) {
+    fileUrl = fileUrl.split("?")[0]
+    let filePath = path.resolve("./public/html" + fileUrl + ".html");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream(filePath).pipe(res);
+    return
+  }
 
+  let filePath = path.resolve("./public/html/" + fileUrl + ".html");
+  const ext = path.extname(filePath);
+  if (ext === ".html") {
+    fs.exists(filePath, (exist) => {
+      if (!exist) {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/html");
+        res.end("<html><body><h1>Eror 404</h1></body></html>");
+        return;
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      fs.createReadStream(filePath).pipe(res);
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><body><h1>Eror 404 not a HTML file</h1></body></html>");
+  }
+});
 
-  module.exports = renderHtml;
+module.exports = renderHtml;
